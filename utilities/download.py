9eb546c3be05111f29c12964e2656025c5e188b1
SpaceCoreOfficial <105853687+SpaceCoreOfficial@users.noreply.github.com>
2023-03-19 17:22:36 +0500

    Create download.py

---
+++ utilities/download.py
@@ -0,0 +1,12 @@
+# import requests, os, sys, zipfile
+
+
+# os.chdir("..")
+
+# input(os.getcwd())
+
+# zipFile = zipfile.ZipFile('main.zip', 'r')
+# zipFile.extractall("Space-Terminal-main/")
+# zipFile.close()
+
+print("This function is not work now, but i will fix this one\nMaybe...")
