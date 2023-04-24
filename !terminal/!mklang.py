c3f616150bfac22aa5e30df1763534e336250ee4
Yo <105853687+SpaceCoreOfficial@users.noreply.github.com>
2023-04-24 19:54:39 +0500

    Update !mklang.py

--- !terminal/!mklang.py
+++ !terminal/!mklang.py
@@ -1,7 +1,7 @@
 import os, sys
 
 
-if os.path.isfile('settings/lang.txt'):
+if os.path.isfile('!settings/lang.txt'):
     pass
 else:
     lang_ask = input("""
@@ -11,17 +11,16 @@ Choose your language:
 \t 1.Русский (Russian)
 \t 2.English (Английский)
 $:""")
-
-    lang01 = open('settings/lang.txt', 'w')
+    lang01 = open('!settings/lang.txt', 'w')
     if lang_ask == '1':
         lang01.write('rus')
     elif lang_ask == '2':
         lang01.write('eng')
     lang01.close()
 
-os.chdir('st')
+os.chdir('!st')
 if os.name == 'nt':
-    os.startfile('terminal.py')
+    os.startfile('!terminal.py')
     sys.exit()
 else:
     if lang_ask == '2':
